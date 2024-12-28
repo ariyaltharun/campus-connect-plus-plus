38fa53e79f1e2e90d1f07e5a3d316bb754ea093b
ariyaltharun <ariyaltharun@gmail.com>
2024-12-28 14:58:31 +0530

    integrate frontend and backend
    * Just changed api endpoints names in frontend code
    * Copy pasted previously incomplete code from nanditha's server code

--- backend/routes/search.py
+++ backend/routes/search.py
@@ -13,10 +13,50 @@ async def find_people(people: str, area: str):
     query1 = "MATCH (n:" + people + ") WHERE '" + area + "' in n.area_of_interest RETURN n.name, n.student_name, n.department, n.designation, n.usn, n.email_id"
     query2 = "MATCH (n) WHERE '" + area + "' in n.area_of_interest RETURN n.name, n.student_name, n.department, n.designation, n.usn, n.email_id"
 
-    # Incomplete
-    # result, meta = db.cypher_query(query)
-    # return result
+    # null results are also handled(only in this function)
+    nodes = [[],[],[],[]]
+    if(people=='ALL'):
+        result, meta = db.cypher_query(query2)
+    else:
+        result, meta = db.cypher_query(query1)
+    print(result)
+    # print(meta)
+    # print(len(result))
+    # result_as_dict = [dict(zip(meta, row)) for row in result]
+    # print(result_as_dict)
 
+    if people == 'FACULTY':
+        for i in range(len(result)):
+            nodes[0].append(result[i][0])
+            nodes[1].append(result[i][2])
+            nodes[2].append(result[i][3])
+            nodes[3].append(result[i][5])
+    elif people == 'STUDENT':
+        for i in range(len(result)):
+            nodes[0].append(result[i][1])
+            nodes[1].append(result[i][2])
+            nodes[2].append(result[i][4])
+            nodes[3].append(result[i][5])
+    else:
+        for i in range(len(result)):
+            # faculty details
+            if(result[i][0] != None):
+                nodes[0].append(result[i][0])
+                nodes[1].append(result[i][2])
+                nodes[2].append(result[i][3])
+                nodes[3].append(result[i][5])
+            # student details
+            if(result[i][1] != None):
+                nodes[0].append(result[i][1])
+                nodes[1].append(result[i][2])
+                nodes[2].append(result[i][4])
+                nodes[3].append(result[i][5])
+
+    if nodes[0] == [] :
+        for i in range(len(nodes)):
+            nodes[i].append('None')
+    print(nodes)
+    return nodes
 
 @router.get("/find_projects")
 async def find_projects(status: str, area: str):
@@ -47,3 +87,95 @@ async def find_students(areaOfInterest: str, skill: str):
     print(result)
     return result
 
+
+@router.get("/all_areas")
+async def all_area_of_interests():
+    query = """
+    MATCH (s:STUDENT) 
+    UNWIND s.area_of_interest AS student_interest 
+    WITH COLLECT(DISTINCT student_interest) AS student_interests
+
+    MATCH (f:FACULTY) 
+    UNWIND f.area_of_interest AS faculty_interest 
+    WITH COLLECT(DISTINCT faculty_interest) AS faculty_interests, student_interests
+
+    MATCH (p:PROJECT)
+    UNWIND p.area_of_interest AS project_interest
+    WITH COLLECT(DISTINCT project_interest) AS project_interests, faculty_interests, student_interests
+
+    WITH student_interests + faculty_interests + project_interests AS all_interests
+    UNWIND all_interests AS interest
+    RETURN DISTINCT interest
+    ORDER BY interest
+    """
+    result, meta = db.cypher_query(query)
+    # print(result)
+    return result
+
+
+@router.get("/all_skills")
+async def all_skills():
+    query = """
+    MATCH(s:STUDENT)
+    UNWIND s.skills AS sk
+    RETURN DISTINCT sk
+    ORDER BY sk
+    """
+    result, meta = db.cypher_query(query)
+    print(result)
+    return result
+
+
+@router.get('/know-team')
+def get_projects(status: str):
+    query1 = """
+    MATCH (p:PROJECT)
+    OPTIONAL MATCH (p)<-[:WORKING_ON]-(s:STUDENT)
+    OPTIONAL MATCH (p)<-[:WORKING_ON]-(f:FACULTY)
+    OPTIONAL MATCH (f)-[:GUIDES]->(s)
+    WITH p, COLLECT(DISTINCT s) AS Students, COLLECT(DISTINCT f) AS Faculties
+    WHERE size(Students) > 0 AND size(Faculties) > 0 AND p.status= $status
+    RETURN p.title AS ProjectTitle, 
+           p.description AS ProjectDescription, 
+           p.status AS ProjectStatus, 
+           [s IN Students | s.student_name] AS Students,
+           [f IN Faculties | f.name] AS Faculties
+    ORDER BY ProjectTitle
+    """
+    query2 = """
+    MATCH (p:PROJECT)
+    OPTIONAL MATCH (p)<-[:WORKING_ON]-(s:STUDENT)
+    OPTIONAL MATCH (p)<-[:WORKING_ON]-(f:FACULTY)
+    OPTIONAL MATCH (f)-[:GUIDES]->(s)
+    WITH p, COLLECT(DISTINCT s) AS Students, COLLECT(DISTINCT f) AS Faculties
+    WHERE size(Students) > 0 AND size(Faculties) > 0 
+    RETURN p.title AS ProjectTitle, 
+           p.description AS ProjectDescription, 
+           p.status AS ProjectStatus, 
+           [s IN Students | s.student_name] AS Students,
+           [f IN Faculties | f.name] AS Faculties
+    ORDER BY ProjectTitle
+    """
+    parameters = {
+        'status': status
+    }
+    # result, meta = db.cypher_query(query2, params=parameters)
+    # print(result)
+    if status =='All':
+        result, meta = db.cypher_query(query2)
+        print(result)
+    else:
+        result, meta = db.cypher_query(query1, params=parameters)
+        print(result)
+    projects = []
+    for record in result:
+        project = {
+            "ProjectTitle": record[0],
+            "ProjectDescription": record[1],
+            "ProjectStatus": record[2],
+            "Students": record[3],
+            "Faculties": record[4]
+        }
+        projects.append(project)
+    print(projects)
+    return projects
